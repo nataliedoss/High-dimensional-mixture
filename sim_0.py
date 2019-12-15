5117d3d0c10850d4ac180416b9554ee098ebb08e
Natalie Doss <natalie.doss@yale.edu>
2019-12-15 16:42:46 -0500

    m

--- sim_0.py
+++ sim_0.py
@@ -27,20 +27,18 @@ def sim_over_d(num_sims, k, ld, num, sigma, d_range, factor_weights, factor_thet
         d = d_range[i]
 
         # One model:
-        #x1 = np.repeat(1, d)
-        #x2 = np.repeat(-1, d)
+        #x1 = np.repeat(1/np.sqrt(d), d)
+        #x2 = np.repeat(-1/np.sqrt(d), d)
         #x = np.array((x1, x2))
         #weights = np.repeat(1.0/k, k)
         # One model:
         x = np.random.uniform(-1.0/np.sqrt(d), 1.0/np.sqrt(d), k*d).reshape(k, d)
         # One model:
-        #x = np.asarray(random.choices([1, -1], k=k*d)).reshape(k, d)
-        # One model:
         #x = np.asarray(random.choices([1/np.sqrt(d), -1/np.sqrt(d)], k=k*d)).reshape(k, d)
 
         weights = np.random.dirichlet(np.repeat(1.0, k), 1).reshape(k, )
         # If you want the true model to be centered:
-        #x_centered = x - np.average(x, axis=0, weights=weights)
+        x_centered = x - np.average(x, axis=0, weights=weights)
         
         u_rv = DiscreteRV_HD(weights, x) # true model
         model = ModelGM_HD(w=weights, x=x, std=sigma)
@@ -83,42 +81,41 @@ def sim_over_d(num_sims, k, ld, num, sigma, d_range, factor_weights, factor_thet
 
 
 
-
 ####################################################################
 # Run sim study
 num_sims = 10
 num = 10000
 sigma = 1.0
 d_range = np.arange(100, 1000, 100)
-factor_weights = 5.0
-factor_thetas = 0.2
+factor_weights = 4.0
+factor_thetas = 0.1
 
 
-sim_k2 = sim_over_d(num_sims=num_sims, k=2, ld=2, num=num,
-                    sigma=sigma, d_range=d_range,
-                    factor_weights=factor_weights, factor_thetas=factor_thetas)
+sim = sim_over_d(num_sims=num_sims, k=3, ld=2, num=num,
+                 sigma=sigma, d_range=d_range,
+                 factor_weights=factor_weights, factor_thetas=factor_thetas)
 
 
 # Plots
 
-# k = 2
+
 # Accuracy
 plt.subplot(1, 2, 1)
 #plt.plot()
-p1 = plt.errorbar(d_range, sim_k2[0], sim_k2[1])
-p2 = plt.errorbar(d_range, sim_k2[2], sim_k2[3])
-plt.title("k = 2: Accuracy as d grows")
+p1 = plt.errorbar(d_range, sim[0], sim[1])
+p2 = plt.errorbar(d_range, sim[2], sim[3])
+plt.title("Accuracy as d grows")
 plt.xlabel("d")
 plt.ylabel("Wasserstein-1")
 plt.legend((p1, p2), ("DMM", "EM"), loc='upper left', shadow=True)
 # Time
 plt.subplot(1, 2, 2)
-p1 = plt.scatter(d_range, sim_k2[4])
-p2 = plt.scatter(d_range, sim_k2[5])
-plt.title("k = 2: Time as d grows")
+p1 = plt.scatter(d_range, sim[4])
+p2 = plt.scatter(d_range, sim[5])
+plt.title("Time as d grows")
 plt.xlabel("d")
-plt.ylabel("Time")
+plt.ylabel("Time in seconds")
 plt.legend((p1, p2), ("DMM", "EM"), loc='upper left', shadow=True)
-plt.savefig("sim_k2.pdf")
+plt.savefig("sim.pdf")
 plt.close()
 
