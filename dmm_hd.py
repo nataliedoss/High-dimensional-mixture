c0df0304880fed4284877b4d614e42519e0c5470
Natalie Doss <natalie.doss@yale.edu>
2020-01-17 15:21:06 -0500

    mle for weights

--- dmm_hd.py
+++ dmm_hd.py
@@ -4,6 +4,7 @@ module for high-dimensional denoised method of moments algorithm
 from dmm import DMM
 from model_gm import ModelGM_HD, sample_gm
 from discrete_rv import DiscreteRV_HD, wass_hd
+from sklearn.metrics import pairwise_distances
 import itertools
 from scipy.special import comb
 import numpy as np
@@ -84,11 +85,11 @@ class DMM_HD():
         return simplex_grid(self.k, factor_weights * rate_inverse) / (factor_weights * rate_inverse)
 
 
-
+    '''
     def generate_net_thetas(self, num, factor_thetas):
         """
-        One method to generate epsilon net on unit vectors in ld space.
-        This method generates (1/epsilon)^ld random Gaussian vectors in ld space and normalizes them. Approximate grid. 
+        One method to generate epsilon net on unit sphere S^{ld-1}.
+        This method generates (factor_thetas/epsilon))^(ld-1) random Gaussian vectors in ld space and normalizes them. Approximate grid. 
         Here epsilon is the rate, so 1/epsilon is rate_inverse.
 
         Args:
@@ -97,15 +98,16 @@ class DMM_HD():
             If > 1.0, makes grid finer.
 
         Returns:
-        Array(float, (size of epsilon net in ld space) x ld).
+        Array(float, (size of net) x ld).
         """
         
         rate_inverse = self.compute_rate_inverse(num)
-        nt = rate_inverse**self.ld
-        thetas = np.random.multivariate_normal(np.zeros(self.ld), np.identity(self.ld), nt)
+        nt = (factor_thetas*rate_inverse)**(self.ld-1)
+        thetas = np.random.multivariate_normal(np.zeros(self.ld), np.identity(self.ld), int(nt))
         return (thetas.T / np.apply_along_axis(np.linalg.norm, 1, thetas)).T
 
-    
+
+
     def generate_net_thetas(self, num, factor_thetas):
         """
         One method to generate epsilon net on unit vectors in ld space.
@@ -118,18 +120,71 @@ class DMM_HD():
             If > 1.0, makes grid finer.
 
         Returns:
-        Array(float, (size of epsilon net in ld space) x ld).
+        Array(float, (size of net) x ld).
         """
 
         rate_inverse = self.compute_rate_inverse(num)
-        grid_1d = np.arange(-1, 1.1, 1.0/(factor_thetas * rate_inverse))
+        grid_1d = np.arange(-1.0, 1.0, 1.0/(factor_thetas * rate_inverse))
         thetas = np.empty((len(grid_1d)**self.ld, self.ld))
         for i in range(len(grid_1d)):
             for j in range(len(grid_1d)):
                 thetas[((i*len(grid_1d)) + j), ] = np.array((grid_1d[i], grid_1d[j]))
         thetas = thetas[~np.all(thetas == 0, axis=1)] # Remove any row with all zeros
+        thetas = (thetas.T / np.apply_along_axis(np.linalg.norm, 1, thetas)).T # Take unit norm
 
-        return np.unique((thetas.T / np.apply_along_axis(np.linalg.norm, 1, thetas)).T, axis=0)
+        return np.unique(thetas.round(decimals=4), axis=0)
+
+    
+    def generate_net_thetas(self, num, factor_thetas):
+        """
+        One method to generate epsilon net on unit vectors in ld space.
+        This method generates a net on the unit rectangle in R^2.
+        It then projects those vectors to the unit sphere.
+        BUT this method only works for self.ld = 2. 
+
+        Args:
+        num: Int. Number of samples in dataset.
+        factor_thetas: Float. factor by which to multiply the rate.
+            If > 1.0, makes grid finer.
+
+        Returns:
+        Array(float, (size of net) x ld).
+        """
+
+        rate_inverse = self.compute_rate_inverse(num)
+        grid_1d = np.arange(-1.0, 1.001, 1.0/(factor_thetas * rate_inverse))
+        top = np.array((grid_1d, np.repeat(1.0, len(grid_1d))))
+        bottom = np.array((grid_1d, np.repeat(-1.0, len(grid_1d))))
+        right = np.array((np.repeat(1.0, len(grid_1d)), grid_1d))
+        left = np.array((np.repeat(-1.0, len(grid_1d)), grid_1d))
+        thetas = np.concatenate((top, bottom, right, left), axis=1)
+        thetas = thetas[~np.all(thetas == 0, axis=1)] # Remove any row with all zeros
+        thetas = (thetas.T / np.apply_along_axis(np.linalg.norm, 1, thetas)) # Take unit norm
+
+        return np.unique(thetas.round(decimals=4), axis=0)
+    '''
+
+
+    def generate_net_thetas(self, num, factor_thetas):
+        """
+        One method to generate epsilon net on unit vectors in ld space.
+        This method generates (1/epsilon) grid points on alpha, then forms the unit vectors (cos alpha, sin alpha).
+        BUT this method only works for self.ld = 2. 
+
+        Args:
+        num: Int. Number of samples in dataset.
+        factor_thetas: Float. factor by which to multiply the rate.
+            If > 1.0, makes grid finer.
+
+        Returns:
+        Array(float, (size of epsilon net in ld space) x ld-1).
+        """
+
+        rate_inverse = self.compute_rate_inverse(num)
+        grid_angle = np.linspace(-np.pi, np.pi, factor_thetas*rate_inverse)
+
+        return  np.array((np.cos(grid_angle), np.sin(grid_angle))).T
+    
    
 
     def generate_candidates(self, sample_ld, net_weights):
@@ -164,6 +219,40 @@ class DMM_HD():
         return candidate_ests
 
 
+    def generate_candidates_mle(self, sample_ld):
+        """
+        Method to generate candidate distributions from one dimensional ones via maximum likelihood selection of weights.
+
+        Args:
+        sample_ld: Array(float, num x ld). 
+
+        Returns:
+        List(DiscreteRV_HD). Atoms are ld dimensional. 
+        Choices for our estimated distributions in the ld space.
+        """
+              
+        mat_centers = np.empty(shape = (self.k, self.ld))
+        n, d = sample_ld.shape
+        for j in range(self.ld):
+            dmm = DMM(self.k, self.sigma)
+            est = dmm.estimate(sample_ld[:, j])
+            mat_centers[:, j] = est.centers
+
+            A = list(itertools.product(*mat_centers.T))
+            S_centers = list(itertools.combinations(A, self.k))
+            candidate_ests = [None] * (len(S_centers))
+            S_centers_arr = np.asarray(S_centers)
+
+        for i in range(len(S_centers_arr)):
+            atoms = S_centers_arr[i, ]
+            M = np.exp(-np.square(pairwise_distances(sample_ld, atoms))/2/(self.sigma**2))
+            M /= M.sum(axis=1)[:, np.newaxis]
+            p = M.sum(axis=0)/n
+            candidate_ests[i] = DiscreteRV_HD(p, S_centers[i])
+
+        return candidate_ests
+
+
     def generate_theta_ests(self, sample_ld, net_thetas):
         """
         Method to perform one dimensional DMM for data projected along net of unit vectors in ld.
@@ -193,7 +282,8 @@ class DMM_HD():
 
         Args:
         sample_ld: Array(float, n x ld).
-        factor: Scalar(float).
+        factor_weights: Scalar(float).
+        factor_thetas: Scalar(float).
 
         Returns:
         Object of class DiscreteRV_HD. Atoms are ld dimensional.
@@ -226,14 +316,37 @@ class DMM_HD():
         return est_selected
 
 
+    def estimate_ld_mle(self, sample_ld):
+        """
+        Method to perform the multivariate denoised method of moments, for any dimension ld.
 
-    def estimate(self, sample, factor_weights, factor_thetas):
+        Args:
+        sample_ld: Array(float, n x ld).
+
+        Returns:
+        Object of class DiscreteRV_HD. Atoms are ld dimensional.
+        """
+
+        candidate_ests = self.generate_candidates_mle(sample_ld)
+        llhood = np.empty(len(candidate_ests))
+        for i in range(len(candidate_ests)):
+            atoms = candidate_ests[i].atoms
+            weights = candidate_ests[i].weights
+            M = np.exp(-np.square(pairwise_distances(sample_ld, atoms))/2/(self.sigma**2)) * weights
+            llhood[i] = np.sum(np.log(M.sum(axis=1)))
+
+        return candidate_ests[np.argmax(llhood)]
+
+
+    def estimate(self, sample, factor_weights, factor_thetas, MLE):
         """
         Method to perform the complete high dimensional DMM algorithm.
 
         Args:
         sample: Array(float, num x d). Sample from d-dim Gaussian mixture model.
-        factor: scalar(float).
+        factor_weights: Scalar(float).
+        factor_thetas: Scalar(float).
+        MLE: Boolean. True if you want to do the MLE method for weights.
 
         Returns:
         Object of class DiscreteRV_HD. Atoms are d dimensional. 
@@ -250,6 +363,10 @@ class DMM_HD():
             est_ld = dmm.estimate(sample_ld)
             est_centers = np.matmul(est_ld.centers.reshape(self.k, self.ld), U_ld.T)
             est = DiscreteRV_HD(est_ld.weights, est_centers)
+
+        if (MLE):
+            est_ld = self.estimate_ld_mle(sample_ld)
+            est = DiscreteRV_HD(est_ld.weights, np.matmul(est_ld.atoms, U_ld.T))
 
         else:
             est_ld = self.estimate_ld(sample_ld, factor_weights, factor_thetas)
