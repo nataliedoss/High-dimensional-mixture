3fbfc399a10a4de9b8fd9e76a8ec289d09fea693
Natalie Doss <natalie.doss@yale.edu>
2020-01-17 15:21:52 -0500

    symm em

--- em.py
+++ em.py
@@ -1,9 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.metrics import pairwise_distances
 
 
-def em(x, k, sigma, iter):
+
+def em(x, k, sigma, max_iter, tol):
     '''
     param:
     x: shape n-by-d
@@ -16,9 +16,10 @@ def em(x, k, sigma, iter):
     p = np.ones(k)/k
 
     i = 0
-    ll_old = 1
+    M = np.exp(-np.square(pairwise_distances(x, mu))/2/(sigma**2)) * p
+    ll_old = np.sum(np.log(M.sum(axis=1)))
     ll_diff = 1
-    while (i < iter and ll_diff > .0001):
+    while (i < max_iter and ll_diff > tol):
         M = np.exp(-np.square(pairwise_distances(x, mu))/2/(sigma**2)) * p
         M /= M.sum(axis=1)[:, np.newaxis]
         p = M.sum(axis=0)/n
@@ -36,6 +37,50 @@ def em(x, k, sigma, iter):
     print("Number of iterations for EM was ", i, "llhood gap for EM was ", ll_diff)
         
     return p, mu
+
+
+
+
+def em_symm(x, sigma, max_iter, tol):
+    '''
+    Args:
+    x: nxd array
+    sigma: float
+    max_iter: int
+    tol: float
+    
+    Returns:
+    Array of floats, 1 x 2
+    '''
+    
+    n, d = x.shape
+    mu = x[np.random.choice(n, 1, replace=True)]
+
+    i = 0
+    M1 = np.exp(-np.square(np.linalg.norm(x-mu, axis=1)) / (2*sigma**2))
+    M2 = np.exp(-np.square(np.linalg.norm(x+mu, axis=1)) / (2*sigma**2))
+    ll_old = np.sum(np.log(0.5*(M1 + M2)))
+    ll_diff = 1
+    while(i < max_iter and ll_diff > tol):
+        coeff = (M2 - M1) / (M2 + M1)
+        mu = (coeff.T @ x)/n
+
+        # Compute log lhood at new estimate
+        M1 = np.exp(-np.square(np.linalg.norm(x-mu, axis=1)) / (2*sigma**2))
+        M2 = np.exp(-np.square(np.linalg.norm(x+mu, axis=1)) / (2*sigma**2))
+        ll_new = np.sum(np.log(0.5*(M1 + M2)))
+
+        # Compute the difference
+        ll_diff = np.abs((ll_new - ll_old)/ll_old)
+        ll_old = ll_new
+        i += 1
+
+    print("Number of iterations for EM was ", i, "llhood gap for EM was ", ll_diff)
+        
+    return np.array((mu, -mu))
+
+    
+
 
 
 
